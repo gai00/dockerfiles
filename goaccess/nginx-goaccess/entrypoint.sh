381be0a577cbbb95e7dcb4ea5f86046800e0f264
Layer <gai00@users.noreply.github.com>
2020-01-09 17:11:40 +0800

    Update entrypoint.sh

--- goaccess/nginx-goaccess/entrypoint.sh
+++ goaccess/nginx-goaccess/entrypoint.sh
@@ -4,7 +4,7 @@ curl -s --unix-socket /var/run/docker.sock \
 "http://v1.40/$GOACCESS_TARGET/logs?stdout=true&follow=1" \
 --output - \
 | cut -b 9- \
-| goaccess --log-format "'$GOACCESS_LOGFORMAT'" $GOACCESS_ARGS \
+| goaccess --log-format "$GOACCESS_LOGFORMAT" $GOACCESS_ARGS \
 --ws-url $GOACCESS_WSURL/ws \
 -o /usr/share/nginx/html/index.html \
 --real-time-html \

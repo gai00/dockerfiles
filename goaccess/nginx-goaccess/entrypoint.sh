e983789cf26b70ec3b523aefbf8236e5748d57ed
Layer <gai00@users.noreply.github.com>
2020-01-09 14:08:15 +0800

    Create entrypoint.sh

---
+++ goaccess/nginx-goaccess/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+nginx -g "daemon on;"
+curl -s --unix-socket /var/run/docker.sock \
+"http://v1.40/$GOACCESS_TARGET/logs?stdout=true&follow=1" \
+--output - \
+| cut -b 9- \
+| goaccess --log-format COMBINED \
+--ws-url $GOACCESS_WSURL/ws \
+-o /usr/share/nginx/html/index.html \
+--real-time-html \
+| tail -f

dd1684bb04f267c656515a066577f891c13876e2
Layer <gai00@users.noreply.github.com>
2020-01-15 12:42:37 +0800

    Update entrypoint.sh

--- goaccess/nginx-goaccess/entrypoint.sh
+++ goaccess/nginx-goaccess/entrypoint.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 nginx -g "daemon on;"
-curl -s --unix-socket /var/run/docker.sock \
-"http://v1.40/$GOACCESS_TARGET/logs?stdout=true&follow=1" \
---output - \
-| cut -b 9- \
+
+mkfifo -m 644 /tmp/goaccess.fifo
+
+tail -f /tmp/goaccess.fifo \
 | goaccess --log-format "$GOACCESS_LOGFORMAT" $GOACCESS_ARGS \
 --ws-url $GOACCESS_WSURL/ws \
 -o /usr/share/nginx/html/index.html \
---real-time-html \
-| tail -f
+--real-time-html &
+
+curl -s --unix-socket /var/run/docker.sock \
+"http://v1.40/$GOACCESS_TARGET/logs?stdout=1" \
+--output - \
+| cut -b 9- > /tmp/goaccess.fifo
+
+while :
+do
+curl -s --unix-socket /var/run/docker.sock \
+"http://v1.40/$GOACCESS_TARGET/logs?stdout=1&tail=0&follow=1" \
+ -m 30 --output - \
+| cut -b 9- > /tmp/goaccess.fifo
+done
